2046526caf374d2766ec0416e86f64f118d779b2
Michael O'Connor <oconnor97@aol.com>
2021-04-06 10:40:10 -0400

    setting tests for the Engineer class

--- test/engineer.test.js
+++ test/engineer.test.js
@@ -1,11 +1,16 @@
 const Engineer = require('../lib/engineer')
 
 
-// tests for the Enineer class
+// tests for the Engineer class
 describe('Engineer', () => {
     it('should return the github userName with the getGitHub function', () => {
-        let github = 'username';
-        let engineerGitHub = new Engineer('Michael', 1, 'test@test.com', github);
-        expect(getGitHub(github)).toBe('username');
+        let userName = 'username';
+        let engineerGitHub = new Engineer('Michael', 1, 'test@test.com', userName);
+        expect(engineerGitHub.getGitHub(userName)).toBe('username');
+    });
+    it('should return the github userName with the getGitHub function', () => {
+        let role = 'Engineer';
+        let engineerRole = new Engineer('Michael', 1, 'test@test.com', 'username');
+        expect(engineerRole.getRole(role)).toBe('Engineer');
     });
 });
