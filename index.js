bb18e367f7a1afd95557d83a50cc330959de061c
Michael O'Connor <oconnor97@aol.com>
2021-04-06 09:47:10 -0400

    restructuring my file content and creating my first test fo rthe employee

---
+++ index.js
@@ -0,0 +1,9 @@
+const Employee = require('/lib/employee')
+const Manager = require('./lib/manager')
+const Engenieer = require('./lib/engineer')
+const Intern = require('./lib/intern')
+
+
+const inquirer = require('inquirer')
+const fs = require('fs')
+
